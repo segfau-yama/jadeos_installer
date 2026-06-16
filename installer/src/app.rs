d255183685a2c2077efd6cd31f3aa3ecdc28b632
segfau-yama <suiki547@gmail.com>
2026-06-17 00:54:13 +0900

    インストーラーフルスクリーン化

--- installer/src/app.rs
+++ installer/src/app.rs
@@ -3,15 +3,15 @@ use dioxus::prelude::*;
 use crate::gui::routes::Route;
 use crate::gui::state::InstallerState;
 
-const TAILWIND_STYLES: Asset = asset!("/assets/tailwind.css");
+const TAILWIND_STYLES: &str = include_str!("../assets/tailwind.css");
 
 pub fn app() -> Element {
     let state = use_signal(InstallerState::default);
     use_context_provider(|| state);
 
     rsx! {
-        document::Stylesheet {
-            href: TAILWIND_STYLES
+        document::Style {
+            {TAILWIND_STYLES}
         }
         Router::<Route> {}
     }
--- installer/src/main.rs
+++ installer/src/main.rs
@@ -1,5 +1,15 @@
 use jade_installer::app::app;
 
 fn main() {
-    dioxus::launch(app);
+    use dioxus::desktop::{tao::window::Fullscreen, Config, WindowBuilder};
+    use dioxus::LaunchBuilder;
+
+    let window = WindowBuilder::new()
+        .with_title("JadeOS Installer")
+        .with_decorations(false)
+        .with_fullscreen(Some(Fullscreen::Borderless(None)));
+
+    LaunchBuilder::desktop()
+        .with_cfg(Config::new().with_window(window))
+        .launch(app);
 }
